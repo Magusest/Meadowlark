5fd485178ac78e58a9e45e020e29c162a1147c36
Magusest <redikor49@gmail.com>
2026-02-15 23:40:05 +0300

    set prettier

--- meadowlark.js
+++ meadowlark.js
@@ -1,38 +1,43 @@
-import express from 'express'
-import { engine } from 'express-handlebars'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import { getFortune } from './lib/fortune.js'
+import express from "express";
+import { engine } from "express-handlebars";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { getFortune } from "./lib/fortune.js";
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Setting view Handlebars
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => res.render('home'))
+app.get("/", (req, res) => res.render("home"));
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune: getFortune()})
-})
+app.get("/about", (req, res) => {
+  res.render("about", { fortune: getFortune() });
+});
 
 // 404 page
 app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+  res.status(404);
+  res.render("404");
+});
 
 // 500 page
 app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+  console.error(err.message);
+  res.status(500);
+  res.render("500");
+});
 
-app.listen(port, () => console.log(`Express has starded on http//localhost:${port}` + ' Press Ctrl + C for end.'))
+app.listen(port, () =>
+  console.log(
+    `Express has starded on http//localhost:${port}` +
+      " Press Ctrl + C for end."
+  )
+);
